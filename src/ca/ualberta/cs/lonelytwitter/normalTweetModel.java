e7474580b34e384fba007bbb17c425174b0aee27
MattDekinder <dekinder@ualberta.ca>
2014-01-14 18:12:17 -0700

    added nomalTweetModel subclass

---
+++ src/ca/ualberta/cs/lonelytwitter/normalTweetModel.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class normalTweetModel extends lonelyTweetModel{
+
+	public normalTweetModel(String text, Date timestamp){
+		super(text, timestamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public normalTweetModel(String text){
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setText(String text){
+		// TODO Auto-generated method stub
+	}
+
+}
